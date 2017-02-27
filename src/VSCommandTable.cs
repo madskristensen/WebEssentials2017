1da6a1aa837c49b5b0b65b0c80b0bd1cc69774f5
Mads Kristensen <post@madskristensen.net>
2017-02-27 11:00:10 -0800

    Updated context menu icon

--- src/VSCommandTable.cs
+++ src/VSCommandTable.cs
@@ -14,8 +14,10 @@ namespace WebEssentials
     {
         public const string guidVSPackageString = "f168228c-63f6-4db0-b426-43c30e9d1fc7";
         public const string guidVSPackageCmdSetString = "9232585f-9c24-4dc7-bce8-985261542acf";
+        public const string iconsString = "26d84633-82d5-440d-af48-28e57bcab783";
         public static Guid guidVSPackage = new Guid(guidVSPackageString);
         public static Guid guidVSPackageCmdSet = new Guid(guidVSPackageCmdSetString);
+        public static Guid icons = new Guid(iconsString);
     }
     /// <summary>
     /// Helper class that encapsulates all CommandIDs uses across VS Package.
@@ -24,5 +26,6 @@ namespace WebEssentials
     {
         public const int MyMenuGroup = 0x1020;
         public const int ResetExtensions = 0x0200;
+        public const int small = 0x0001;
     }
 }
