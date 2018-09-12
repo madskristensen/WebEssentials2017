9f301e9fee195064926e1154e4104fface2fe3ec
Mads Kristensen <post@madskristensen.net>
2018-09-12 11:44:12 -0700

    Workaround for extension packs /cc #38 and #39

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -5,14 +5,14 @@
 // ------------------------------------------------------------------------------
 namespace WebEssentials
 {
-				static class Vsix
-				{
-								public const string Id = "bb7e2273-9a70-4e5e-b4dd-1f361b6166c0";
-								public const string Name = "Web Essentials 2017";
-								public const string Description = @"The easiest way to set up Visual Studio for the ultimate web development experience. Requires Visual Studio 2017 Update 3 or newer";
-								public const string Language = "en-US";
-								public const string Version = "2.0";
-								public const string Author = "Mads Kristensen";
-								public const string Tags = "extensions, vsix, install";
-				}
+    static class Vsix
+    {
+        public const string Id = "bb7e2273-9a70-4e5e-b4dd-1f361b6166c0";
+        public const string Name = "Web Essentials 2017";
+        public const string Description = @"The easiest way to set up Visual Studio for the ultimate web development experience. Requires Visual Studio 2017 Update 3 or newer";
+        public const string Language = "en-US";
+        public const string Version = "2.0";
+        public const string Author = "Mads Kristensen";
+        public const string Tags = "extensions, vsix, install";
+    }
 }
